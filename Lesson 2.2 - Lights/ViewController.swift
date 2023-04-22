861d478e790ad30c0395777a23f8ef8006526dda
Alexey Zinovyev <zinovkin@gmail.com>
2023-04-22 19:01:17 +0300

    Initial Commit

---
+++ Lesson 2.2 - Lights/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson 2.2 - Lights
+//
+//  Created by Alexey Zinoviev on 22.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
